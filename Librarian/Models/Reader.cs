a5ddb59e1b79b99a27a0ee9c505b56bc3b4cd70a
alberteks <albert_eks@outlook.com>
2023-03-27 18:39:02 -0700

    Added the reader class

---
+++ Librarian/Models/Reader.cs
@@ -0,0 +1,11 @@
+﻿namespace Librarian.Models
+{
+    public class Reader
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public int BooksBorrowed { get; set; } = 0;
+    }
+}
