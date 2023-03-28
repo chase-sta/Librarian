2ad9c140d23f8b100b0cccbe62085f41cbaf61a4
admin <chase.stancik@gmail.com>
2023-03-27 19:29:23 -0600

    Books class created

---
+++ Librarian/Models/Books.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Librarian.Models
+{
+	public class Books
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+
+        public bool IsAvailble { get; set; }
+
+        public string Description { get; set; }
+        
+    }
+}
+
