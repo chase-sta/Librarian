55b1537b2c3a168d89eb889a96484af10b90872e
admin <chase.stancik@gmail.com>
2023-04-03 19:01:03 -0600

    DataAnnotations added to Reader class

--- Librarian/Models/Reader.cs
+++ Librarian/Models/Reader.cs
@@ -1,11 +1,21 @@
-﻿namespace Librarian.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Librarian.Models
 {
     public class Reader
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public int Age { get; set; }
+
+        [EmailAddressAttribute]
         public string Email { get; set; }
+
         public int BooksBorrowed { get; set; } = 0;
     }
 }
